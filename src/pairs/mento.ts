01ee16593f7fb2a5bd5d9b95563e1c36d931b46f
Zviad Metreveli <thezviad@gmail.com>
2021-10-16 13:18:48 +0000

    mento: add support for detecting bucket updates

--- src/pairs/mento.ts
+++ src/pairs/mento.ts
@@ -3,9 +3,14 @@ import { ExchangeWrapper } from "@celo/contractkit/lib/wrappers/Exchange";
 import BigNumber from "bignumber.js";
 import { Address, PairXYeqK } from "../pair";
 import { address as pairMentoAddress } from "../../tools/deployed/mainnet.PairMento.addr.json"
+import { ReserveWrapper } from "@celo/contractkit/lib/wrappers/Reserve";
+import { SortedOraclesWrapper } from "@celo/contractkit/lib/wrappers/SortedOracles";
+import { StableTokenContract } from "@celo/contractkit/lib/base";
 
 export class PairMento extends PairXYeqK {
 	private exchange?: ExchangeWrapper
+	private reserve?: ReserveWrapper
+	private sortedOracles?: SortedOraclesWrapper
 
 	constructor(
 		private kit: ContractKit,
@@ -29,21 +34,63 @@ export class PairMento extends PairXYeqK {
 			throw new Error(`invalid cSTB: ${this.tokenB} !== ${cSTB.address}`)
 		}
 		this.exchange = await this.kit.contracts.getExchange(this.stableToken)
+		this.reserve = await this.kit.contracts.getReserve()
+		this.sortedOracles = await this.kit.contracts.getSortedOracles()
 	}
 
 	public async refresh(): Promise<void> {
-		if (!this.exchange) {
-			throw new Error(`not initialized!`)
-		}
-		// TODO(zviad): Detect upcoming bucket change.
 		const [
+			lastUpdateSecs,
+			updateFrequencySecs,
 			spread,
-			[bucketCELO, bucketSTB],
 		] = await Promise.all([
-			this.exchange.spread(),
-			this.exchange.getBuyAndSellBuckets(false),
+			this.exchange!.lastBucketUpdate(),
+			this.exchange!.updateFrequency(),
+			this.exchange!.spread(),
 		])
-		this.refreshBuckets(new BigNumber(1).minus(spread), bucketCELO, bucketSTB)
+		const tillUpdateSecs = lastUpdateSecs.plus(updateFrequencySecs).minus(Date.now() / 1000)
+		let buckets: {bucketCELO: BigNumber, bucketSTB: BigNumber}
+		if (tillUpdateSecs.gt(0) && tillUpdateSecs.lte(5)) {
+			// Next block will likely have bucket update. `getBuyAndSellBuckets` will be inaccurate
+			// because block timestamp in next block will be 0-5 seconds in future.
+			buckets = await this.mentoBucketsAfterUpdate()
+		} else {
+			const [bucketCELO, bucketSTB] = await this.exchange!.getBuyAndSellBuckets(false)
+			buckets = {bucketCELO, bucketSTB}
+		}
+		this.refreshBuckets(new BigNumber(1).minus(spread), buckets.bucketCELO, buckets.bucketSTB)
+	}
+
+	private mentoBucketsAfterUpdate = async () => {
+		// ## from Exchange.sol:
+		// function getUpdatedBuckets() private view returns (uint256, uint256) {
+		// 	uint256 updatedGoldBucket = getUpdatedGoldBucket();
+		// 	uint256 exchangeRateNumerator;
+		// 	uint256 exchangeRateDenominator;
+		// 	(exchangeRateNumerator, exchangeRateDenominator) = getOracleExchangeRate();
+		// 	uint256 updatedStableBucket = exchangeRateNumerator.mul(updatedGoldBucket).div(
+		// 		exchangeRateDenominator
+		// 	);
+		// 	return (updatedGoldBucket, updatedStableBucket);
+		// }
+		//
+		// function getUpdatedGoldBucket() private view returns (uint256) {
+		// 	uint256 reserveGoldBalance = getReserve().getUnfrozenReserveGoldBalance();
+		// 	return reserveFraction.multiply(FixidityLib.newFixed(reserveGoldBalance)).fromFixed();
+		// }
+		const stableContract = this.kit.celoTokens.getContract(this.stableToken)
+		const [
+			reserveGoldBalance,
+			reserveFraction,
+			oracleRate,
+		] = await Promise.all([
+			this.reserve!.getUnfrozenReserveCeloBalance(),
+			this.exchange!.reserveFraction(),
+			this.sortedOracles!.medianRate(stableContract),
+		])
+		const bucketCELO = reserveGoldBalance.multipliedBy(reserveFraction).integerValue(BigNumber.ROUND_DOWN)
+		const bucketSTB = bucketCELO.multipliedBy(oracleRate.rate).integerValue(BigNumber.ROUND_DOWN)
+		return {bucketCELO, bucketSTB}
 	}
 
 	public swapData() {
