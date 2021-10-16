a1f185a9728a1d3e5604037488a120e27acc7475
Zviad Metreveli <thezviad@gmail.com>
2021-10-16 11:36:05 +0000

    re-export more in the index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-export * from "./swappa-manager"
+export * as SwappaManager from "./swappa-manager"
+export * from "./pair"
+export * from "./registry"
+export * from "./router"
 export * from "./registries/aave"
 export * from "./registries/mento"
 export * from "./registries/uniswapv2"
+export { address as swappaRouterV1Address } from '../tools/deployed/mainnet.SwappaRouterV1.addr.json'
