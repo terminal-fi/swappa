931c35e8cf7d3f70af951099571da69c54afb1f5
Zviad Metreveli <thezviad@gmail.com>
2021-10-18 02:35:32 +0000

    export mobius registry

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,5 @@ export * from "./router"
 export * from "./registries/aave"
 export * from "./registries/mento"
 export * from "./registries/uniswapv2"
+export * from "./registries/mobius"
 export { address as swappaRouterV1Address } from '../tools/deployed/mainnet.SwappaRouterV1.addr.json'
