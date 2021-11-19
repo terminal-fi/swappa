8cca3baa9671d2c411837891e6a1558d0563fbf6
Zviad Metreveli <thezviad@gmail.com>
2021-11-19 03:01:51 +0000

    improve cli.ts and add simple docmentation in README

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import commander from 'commander';
-import { newKit } from '@celo/contractkit';
+import { ContractKit, newKit } from '@celo/contractkit';
 import BigNumber from 'bignumber.js';
 import { toTransactionObject } from '@celo/connect';
 
@@ -15,13 +15,17 @@ import {
 	mainnetRegistryCeloDex,
 } from '../registry-cfg';
 import { RegistryMento } from '../registries/mento';
+import { Registry } from '../registry';
 
 const program = commander.program
 	.option("--network <network>", "Celo client URL to connect to.", "http://localhost:8545")
-	.option("--input <input>", "Input token address.")
-	.option("--output <output>", "Output token address.")
-	.option("--amount <amount>", "Input amount.", "0.001e18")
-	.option("--from <from>", "Account to execute trade from.")
+	.option("--registries <registries>", "Registries to use for routing.", "all")
+	.option("--input <input>", "Input token address or symbol.")
+	.option("--output <output>", "Output token address or symbol.")
+	.option("--amount <amount>", "Input amount.", "0.001")
+	.option("--max-swaps <max-swaps>", "Maximum number of swaps in a route.")
+	.option("--from <from>", "If provided, will actally execute trade from given account.")
+	.option("--max-slippage <max-slippage>", "Maximum allowed slippage.", "0.9999")
 	.parse(process.argv)
 
 process.on('unhandledRejection', (reason: any, _promise: any) => {
@@ -30,52 +34,67 @@ process.on('unhandledRejection', (reason: any, _promise: any) => {
 	process.exit(1)
 })
 
+const registriesByName: {[name: string]: (kit: ContractKit) => Registry} = {
+	"mento": (kit: ContractKit) =>	new RegistryMento(kit),
+	"ubeswap":	mainnetRegistryUbeswap,
+	"sushiswap":	mainnetRegistrySushiswap,
+	"mobius":	mainnetRegistryMobius,
+	"moola":	mainnetRegistryMoola,
+	"moola-v2":	mainnetRegistryMoolaV2,
+	"savingscelo":	mainnetRegistrySavingsCELO,
+	"celodex":	mainnetRegistryCeloDex,
+}
+
+function tokenByAddrOrSymbol(addressOrSymbol: string) {
+	const t = ubeswapTokens.tokens.find((t) => t.address === addressOrSymbol || t.symbol === addressOrSymbol)
+	if (!t) {
+		throw new Error(`Unrecognized token: ${addressOrSymbol}!`)
+	}
+	return t
+}
+
 async function main() {
 	const opts = program.opts()
 	const kit = await newKit(opts.network)
 	const chainId = await kit.web3.eth.getChainId()
 
 	const tokenWhitelist = ubeswapTokens.tokens.filter((v) => v.chainId === chainId).map((v) => v.address)
-	const inputToken = opts.input
-	if (tokenWhitelist.indexOf(inputToken) < 0) {
-		throw new Error("invalid --input token!")
-	}
-	const outputToken = opts.output
-	if (tokenWhitelist.indexOf(outputToken) < 0) {
-		throw new Error("invalid --output token!")
-	}
-	const inputAmount = new BigNumber(opts.amount)
-	const registries = [
-		new RegistryMento(kit),
-		mainnetRegistryUbeswap(kit),
-		mainnetRegistrySushiswap(kit),
-		mainnetRegistryMobius(kit),
-		mainnetRegistryMoola(kit),
-		mainnetRegistryMoolaV2(kit),
-		mainnetRegistrySavingsCELO(kit),
-		mainnetRegistryCeloDex(kit),
-	]
+	const inputToken = tokenByAddrOrSymbol(opts.input)
+	const outputToken = tokenByAddrOrSymbol(opts.output)
+	const inputAmount = new BigNumber(opts.amount).shiftedBy(inputToken.decimals)
+
+	const registryFs = opts.registries === "all" ?
+		Object.values(registriesByName) :
+		(opts.registries as string).split(",").map((x) => registriesByName[x])
+	const registries = registryFs.map((f) => f(kit))
 	const manager = new SwappaManager(kit, swappaRouterV1Address, registries)
 	console.info(`Finding & initializing pairs...`)
 	const pairs = await manager.reinitializePairs(tokenWhitelist)
 	console.info(`Pairs (${pairs.length}):`)
 	for (const pair of pairs) {
-		console.info(`${(pair as any).constructor?.name}:${pair.pairKey}: ${pair.tokenA} / ${pair.tokenB}`)
+		console.info(
+			`${(pair as any).constructor?.name}:${pair.pairKey}: ` +
+			`${tokenByAddrOrSymbol(pair.tokenA).symbol} / ${tokenByAddrOrSymbol(pair.tokenB).symbol}`)
 	}
 
-	console.info(`Finding routes...`)
+	console.info(`--------------------------------------------------------------------------------`)
+	console.info(`Routes: ${inputAmount.shiftedBy(-inputToken.decimals)} ${inputToken.symbol} -> ${outputToken.symbol}...`)
 	const startT0 = Date.now()
 	const routes = manager.findBestRoutesForFixedInputAmount(
-		inputToken,
-		outputToken,
+		inputToken.address,
+		outputToken.address,
 		inputAmount,
+		{
+			maxSwaps: opts.maxSwaps || undefined,
+		}
 	)
 
 	console.info(`Top 5 routes (elapsed: ${Date.now() - startT0}ms):`)
 	for (const route of routes.slice(0, 5)) {
-		const path = route.pairs.map((p, idx) => `${(p as any).constructor.name}:${route.path[idx + 1]}`)
-		const outInfo = ubeswapTokens.tokens.find((t) => t.address === outputToken)
-		console.info(`Output: ${route.outputAmount.shiftedBy(-(outInfo?.decimals || 18)).toFixed(6)} ${outInfo?.symbol}, ${path.join(" -> ")}`)
+		const path = route.pairs.map((p, idx) => `${tokenByAddrOrSymbol(route.path[idx]).symbol}:${(p as any).constructor.name}`)
+		console.info(
+			`Output: ${route.outputAmount.shiftedBy(-(outputToken.decimals || 18)).toFixed(6)} -- ` +
+			`${path.join(":")}:${outputToken.symbol}`)
 	}
 
 	const from = opts.from
@@ -88,14 +107,14 @@ async function main() {
 			const approveTX = toTransactionObject(
 				kit.connection,
 				inputTKN.methods.approve(manager.routerAddr, inputAmount.toFixed(0)))
-			console.info(`sending approve TX...`)
+			console.info(`Sending approve TX...`)
 			const approveReceipt = await approveTX.sendAndWaitForReceipt({from: from})
 			console.info(`TX Done: ${approveReceipt.transactionHash}`)
 		}
 
 		const tx = manager.swap(
-			route, inputAmount, route.outputAmount.multipliedBy(0.9999), from)
-		console.info(`sending TX...`)
+			route, inputAmount, route.outputAmount.multipliedBy(opts.maxSlippage), from)
+		console.info(`Sending TX...`)
 		const receipt = await tx.sendAndWaitForReceipt({from: from})
 		console.info(`TX Done: ${receipt.transactionHash}`)
 	}
