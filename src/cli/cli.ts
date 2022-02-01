6b89b10d84dc4edf204a1480120e9d73983c0e30
Di Wu <dw323@cornell.edu>
2022-02-01 11:07:08 -0800

    fix

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -99,7 +99,7 @@ async function main() {
 	for (const route of routes.slice(0, 5)) {
 		const path = route.pairs.map((p, idx) => `${tokenByAddrOrSymbol(route.path[idx]).symbol}:${(p as any).constructor.name}`)
 		console.info(
-			`Output: ${route.outputAmount.shiftedBy(-(outputToken.decimals || 18)).toFixed(10)} -- ` +
+			`Output: ${route.outputAmount.shiftedBy(-(outputToken.decimals || 18)).toFixed(6)} -- ` +
 			`${path.join(":")}:${outputToken.symbol}`)
 	}
 
