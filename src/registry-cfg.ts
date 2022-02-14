3bb25d275e5a706a9dcb77024d51fcb60ccb21ab
Di Wu <dw323@cornell.edu>
2022-02-14 14:30:57 -0800

    use helper contract for uniswap discovery

--- src/registry-cfg.ts
+++ src/registry-cfg.ts
@@ -16,9 +16,9 @@ import { RegistryBalancer } from "./registries/balancer"
 export const mainnetRegistryMoola =
 	(kit: ContractKit) => new RegistryAave("moola", kit, "0x7AAaD5a5fa74Aec83b74C2a098FBC86E17Ce4aEA")
 export const mainnetRegistryUbeswap =
-	(kit: ContractKit) => new RegistryUniswapV2("ubeswap", kit.web3 as unknown as Web3, "0x62d5b84bE28a183aBB507E125B384122D2C25fAE")
+	(kit: ContractKit) => new RegistryUniswapV2("ubeswap", kit.web3 as unknown as Web3, "0x62d5b84bE28a183aBB507E125B384122D2C25fAE", {registryHelperAddr: "0xA4E880ea9044Ff56Ad7a1a1A74be2Fb5d6B49c3A"})
 export const mainnetRegistrySushiswap =
-	(kit: ContractKit) => new RegistryUniswapV2("sushiswap", kit.web3 as unknown as Web3, "0xc35DADB65012eC5796536bD9864eD8773aBc74C4")
+	(kit: ContractKit) => new RegistryUniswapV2("sushiswap", kit.web3 as unknown as Web3, "0xc35DADB65012eC5796536bD9864eD8773aBc74C4", {registryHelperAddr: "0xA4E880ea9044Ff56Ad7a1a1A74be2Fb5d6B49c3A"})
 export const mainnetRegistryMobius =
 	(kit: ContractKit) => {
 		const web3 = kit.web3 as unknown as Web3
@@ -69,7 +69,7 @@ export const mainnetRegistryMoolaV2 =
 export const mainnetRegistryCeloDex =
 	(kit: ContractKit) => new RegistryUniswapV2("celodex", kit.web3 as unknown as Web3, "0x31bD38d982ccDf3C2D95aF45a3456d319f0Ee1b6", {
 		fixedFee: new BigNumber(0.997), // TODO(zviadm): Figure out actual fee for CeloDex pairs.
-		fetchUsingAllPairs: true,
+		registryHelperAddr: "0xA4E880ea9044Ff56Ad7a1a1A74be2Fb5d6B49c3A"
 	})
 export const mainnetRegistrySymmetric =
 	(kit: ContractKit) => new RegistryBalancer("symmetric", kit.web3 as unknown as Web3, "0x3E30b138ecc85cD89210e1A19a8603544A917372")
