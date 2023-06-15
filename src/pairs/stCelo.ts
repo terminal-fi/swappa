c57679201ce998f6b8d02eeaead737c1449d4a4b
Dylan Mooers <dylmooers@gmail.com>
2023-06-15 10:41:52 -0500

    feat: PairStCelo && PairRstCelo

---
+++ src/pairs/stCelo.ts
@@ -0,0 +1,168 @@
+import Web3 from "web3";
+import { Address, BigNumberString, Pair, Snapshot } from "../pair";
+import {
+  IStakedCelo,
+  ABI as StakedCeloABI,
+} from "../../types/web3-v1-contracts/IStakedCelo";
+import {
+  IAccount,
+  ABI as AccountABI,
+} from "../../types/web3-v1-contracts/IAccount";
+import { address as pairStCeloAddress } from "../../tools/deployed/mainnet.PairStCelo.addr.json";
+import { address as pairRstCeloAddress } from "../../tools/deployed/mainnet.PairRstCelo.addr.json";
+
+import { selectAddress } from "../utils";
+import BigNumber from "bignumber.js";
+
+interface PairStakedCeloSnapshot extends Snapshot {
+  stCeloSupply: BigNumberString;
+  celoBalance: BigNumberString;
+}
+
+abstract class PairStakedCelo extends Pair {
+  allowRepeats = true;
+
+  private stCeloContract: IStakedCelo;
+  private accountContract: IAccount;
+  private stCeloSupply: bigint = BigInt(0);
+  private celoBalance: bigint = BigInt(0);
+
+  constructor(
+    private web3: Web3,
+    swappaPairAddress: Address,
+    private stakedCeloAddress: Address,
+    private accountAddress: Address
+  ) {
+    super(swappaPairAddress);
+    this.stCeloContract = new this.web3.eth.Contract(
+      StakedCeloABI,
+      this.stakedCeloAddress
+    ) as unknown as IStakedCelo;
+    this.accountContract = new this.web3.eth.Contract(
+      AccountABI,
+      this.accountAddress
+    ) as unknown as IAccount;
+  }
+
+  protected async _fetchSupplies() {
+    const [stCeloSupply, celoBalace] = await Promise.all([
+      this.stCeloContract.methods.totalSupply().call(),
+      this.accountContract.methods.getTotalCelo().call(),
+    ]);
+    this.stCeloSupply = BigInt(stCeloSupply.toString());
+    this.celoBalance = BigInt(celoBalace.toString());
+  }
+  public async refresh(): Promise<void> {
+    await this._fetchSupplies();
+  }
+
+  protected toStakedCelo(celoAmount: bigint): bigint {
+    return (celoAmount * this.stCeloSupply) / this.celoBalance;
+  }
+
+  protected toCelo(stCeloAmount: bigint): bigint {
+    return (stCeloAmount * this.celoBalance) / this.celoBalance;
+  }
+
+  public snapshot(): PairStakedCeloSnapshot {
+    return {
+      stCeloSupply: this.stCeloSupply.toString(),
+      celoBalance: this.celoBalance.toString(),
+    };
+  }
+
+  public restore({ stCeloSupply, celoBalance }: PairStakedCeloSnapshot): void {
+    this.stCeloSupply = BigInt(stCeloSupply);
+    this.celoBalance = BigInt(celoBalance);
+  }
+}
+
+export class PairStCelo extends PairStakedCelo {
+  constructor(
+    chainId: number,
+    web3: Web3,
+    accountAddress: Address,
+    private managerAddress: Address,
+    private celoAddr: Address,
+    private stCeloAddr: Address
+  ) {
+    super(
+      web3,
+      selectAddress(chainId, { mainnet: pairStCeloAddress }),
+      stCeloAddr,
+      accountAddress
+    );
+  }
+
+  protected async _init() {
+    return {
+      pairKey: this.managerAddress,
+      tokenA: this.celoAddr,
+      tokenB: this.stCeloAddr,
+    };
+  }
+
+  public outputAmount(inputToken: string, inputAmount: BigNumber): BigNumber {
+    const bnOutput = this._outputAmount(
+      inputToken,
+      BigInt(inputAmount.toFixed(0))
+    );
+
+    return new BigNumber(bnOutput.toString());
+  }
+
+  private _outputAmount(inputToken: string, inputAmount: bigint): bigint {
+    if (inputToken === this.tokenB) return BigInt(0);
+    return this.toStakedCelo(inputAmount);
+  }
+
+  protected swapExtraData(): string {
+    return this.managerAddress;
+  }
+}
+
+export class PairRebasedStCelo extends PairStakedCelo {
+  constructor(
+    chainId: number,
+    web3: Web3,
+    accountAddress: Address,
+    private rstCeloAddr: Address,
+    private stCeloAddr: Address
+  ) {
+    super(
+      web3,
+      selectAddress(chainId, { mainnet: pairRstCeloAddress }),
+      stCeloAddr,
+      accountAddress
+    );
+  }
+
+  protected async _init() {
+    await this._fetchSupplies();
+    return {
+      pairKey: this.rstCeloAddr,
+      tokenA: this.rstCeloAddr,
+      tokenB: this.stCeloAddr,
+    };
+  }
+
+  public outputAmount(inputToken: string, inputAmount: BigNumber): BigNumber {
+    const bnOutput = this._outputAmount(
+      inputToken,
+      BigInt(inputAmount.toFixed(0))
+    );
+
+    return new BigNumber(bnOutput.toString());
+  }
+
+  private _outputAmount(inputToken: string, inputAmount: bigint): bigint {
+    if (inputToken === this.tokenB) return this.toCelo(inputAmount);
+    return this.toStakedCelo(inputAmount);
+  }
+
+  protected swapExtraData(inputToken: string): string {
+    const swapType = inputToken === this.tokenA ? "01" : "02";
+
+    return `${this.rstCeloAddr}${swapType}`;
+  }
+}
