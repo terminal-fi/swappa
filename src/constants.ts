2f601db2d757e070c542f762de4627c58023066e
Zviad Metreveli <thezviad@gmail.com>
2023-07-15 00:56:55 +0000

    rm unneeded constant

--- src/constants.ts
+++ src/constants.ts
@@ -10,16 +10,3 @@ export const Q192 = Q96 ** BigInt(2);
 export const MaxUint256 = BigInt(
   "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
 );
-
-export const KNOWN_FEE_ON_TRANSFER_TOKENS: Partial<{
-  [chain: number]: string[];
-}> = {
-  [42220]: [
-    "0x22401536505dd5d85F7d57f8B37172feDa8f499d",
-    "0xD90BBdf5904cb7d275c41f897495109B9A5adA58",
-    "0x918146359264C492BD6934071c6Bd31C854EDBc3",
-    "0xE273Ad7ee11dCfAA87383aD5977EE1504aC07568",
-    "0x7D00cd74FF385c955EA3d79e47BF06bD7386387D",
-    "0x9802d866fdE4563d088a6619F7CeF82C0B991A55",
-  ].map((s) => s.toLowerCase()),
-};
