875af617a46fce3d994e6182383e7cce15e16742
Zviad Metreveli <thezviad@gmail.com>
2023-07-17 19:27:01 +0000

    re-export mentov2 registry too

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * from "./router"
 export * from "./registries/static"
 export * from "./registries/aave"
 export * from "./registries/mento"
+export * from "./registries/mento-v2"
 export * from "./registries/uniswapv2"
 export * from "./registry-cfg"
 export { address as swappaRouterV1Address } from '../tools/deployed/mainnet.SwappaRouterV1.addr.json'
