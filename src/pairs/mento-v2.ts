87d867b2ffa306d1aa9c76720a521cec60dd6d6c
Zviad Metreveli <thezviad@gmail.com>
2023-07-20 13:28:36 +0000

    mento-v2 implement trading limit support

--- src/pairs/mento-v2.ts
+++ src/pairs/mento-v2.ts
@@ -9,10 +9,11 @@ import { BigNumber } from "bignumber.js";
 import { ethers, providers } from "ethers";
 import Web3 from "web3"
 
-import { IBiPoolManager as IBiPoolManagerW3, ABI as IBiPoolManagerABI } from "../../types/web3-v1-contracts/IBiPoolManager"
+import { newIBiPoolManager } from "../../types/web3-v1-contracts/IBiPoolManager"
 import { Address, Pair, Snapshot, BigNumberString } from "../pair";
 import { selectAddress } from "../utils";
 import { address as mainnetPairMentoV2Address } from "../../tools/deployed/mainnet.PairMentoV2.addr.json";
+import { newErc20 } from "../../types/web3-v1-contracts/ERC20";
 
 enum PricingFunctionType {
   ConstantProduct = "ConstantProduct",
@@ -27,6 +28,7 @@ interface PairMentoV2Snapshot extends Snapshot {
   bucket0: BigNumberString;
   bucket1: BigNumberString;
   tokenPrecisionMultipliers: BigNumberString[],
+  decimals: [number, number],
 }
 
 export class PairMentoV2 extends Pair {
@@ -34,9 +36,13 @@ export class PairMentoV2 extends Pair {
   private spread: BigNumber = new BigNumber(0);
   private updateFrequency: BigNumber = new BigNumber(0);
   private pricingModule!: PricingFunctionType;
+  private tokenPrecisionMultipliers: BigNumber[] = []
+  private decimals: [number, number] = [0, 0]
+
   private bucket0: BigNumber = new BigNumber(0);
   private bucket1: BigNumber = new BigNumber(0);
-  private tokenPrecisionMultipliers: BigNumber[] = []
+  private tokenMaxIn = [new BigNumber(0), new BigNumber(0)]
+  private tokenMaxOut = [new BigNumber(0), new BigNumber(0)]
 
   private provider: ethers.providers.Provider;
   private biPoolManager: IBiPoolManager
@@ -59,9 +65,17 @@ export class PairMentoV2 extends Pair {
     tokenB: string;
   }> {
     this.poolExchange = await this.biPoolManager.getPoolExchange(this.exchange.id);
-    const managerW3 = new this.web3.eth.Contract(IBiPoolManagerABI, this.exchange.providerAddr) as unknown as IBiPoolManagerW3
+    const managerW3 = newIBiPoolManager(this.web3, this.exchange.providerAddr)
     this.tokenPrecisionMultipliers = await Promise.all(
       this.exchange.assets.map((asset) => managerW3.methods.tokenPrecisionMultipliers(asset).call().then((v) => new BigNumber(v))))
+    const [
+      decimalsA,
+      decimalsB,
+    ] = await Promise.all([
+      newErc20(this.web3, this.exchange.assets[0]).methods.decimals().call(),
+      newErc20(this.web3, this.exchange.assets[1]).methods.decimals().call(),
+    ])
+    this.decimals = [Number.parseInt(decimalsA), Number.parseInt(decimalsB)]
     this.pricingModule = await this.getPricingModuleName(this.poolExchange.pricingModule);
     return {
       pairKey: this.exchange.id,
@@ -71,25 +85,45 @@ export class PairMentoV2 extends Pair {
   }
 
   public async refresh() {
-    this.poolExchange = await this.biPoolManager.getPoolExchange(this.exchange.id);
+    const [
+      poolExchange,
+      tradingLimits,
+    ] = await Promise.all([
+      this.biPoolManager.getPoolExchange(this.exchange.id),
+      this.mento.getTradingLimits(this.exchange.id),
+    ])
+    this.poolExchange = poolExchange
     this.spread = new BigNumber(this.poolExchange.config.spread.value._hex);
     this.updateFrequency = new BigNumber(this.poolExchange.config.referenceRateResetFrequency._hex);
     const lastBucketUpdate = new BigNumber(this.poolExchange.lastBucketUpdate._hex);
     const tillUpdateSecs = lastBucketUpdate
       .plus(this.updateFrequency)
       .minus(Date.now() / 1000);
-    let buckets: { bucket0: BigNumber; bucket1: BigNumber };
     if (tillUpdateSecs.lte(5)) {
-      buckets = await this.mentoBucketsAfterUpdate();
+      const buckets = await this.mentoBucketsAfterUpdate();
+      this.bucket0 = buckets.bucket0
+      this.bucket1 = buckets.bucket1
     } else {
-      const [bucket0, bucket1] = [
+      ;[this.bucket0, this.bucket1] = [
         new BigNumber(this.poolExchange.bucket0._hex),
         new BigNumber(this.poolExchange.bucket1._hex),
       ];
-      buckets = { bucket0, bucket1 };
     }
-    this.bucket0 = buckets.bucket0
-    this.bucket1 = buckets.bucket1
+
+    const maxU256 = "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+    this.tokenMaxIn = [new BigNumber(maxU256), new BigNumber(maxU256)]
+    this.tokenMaxOut = [new BigNumber(maxU256), new BigNumber(maxU256)]
+    tradingLimits.forEach((l) => {
+      const idx = l.asset === this.tokenA ? 0 : 1
+      const maxIn = new BigNumber(l.maxIn).shiftedBy(this.decimals[idx])
+      const maxOut = new BigNumber(l.maxOut).shiftedBy(this.decimals[idx])
+      if (this.tokenMaxIn[idx].gt(maxIn)) {
+        this.tokenMaxIn[idx] = maxIn
+      }
+      if (this.tokenMaxOut[idx].gt(maxOut)) {
+        this.tokenMaxOut[idx] = maxOut
+      }
+    })
   }
 
   public swapExtraData(): string {
@@ -101,6 +135,11 @@ export class PairMentoV2 extends Pair {
     const getAmountOut = GET_AMOUNT_OUT[this.pricingModule];
     const [tokenInBucketSize, tokenOutBucketSize] =
       (inputToken === this.tokenA) ? [this.bucket0, this.bucket1] : [this.bucket1, this.bucket0];
+    const [tokenMaxIn, tokenMaxOut] =
+      (inputToken === this.tokenA) ? [this.tokenMaxIn[0], this.tokenMaxOut[1]] : [this.tokenMaxIn[1], this.tokenMaxOut[0]]
+    if (inputAmount.gt(tokenMaxIn)) {
+      return new BigNumber(0)
+    }
 
     const [inputMultiplier, outputMultiplier] =
       (inputToken === this.tokenA) ?
@@ -113,7 +152,11 @@ export class PairMentoV2 extends Pair {
       this.spread,
       scaledInputAmount,
     );
-    return amountOut.idiv(outputMultiplier);
+    const outputAmount = amountOut.idiv(outputMultiplier);
+    if (outputAmount.gt(tokenMaxOut)) {
+      return new BigNumber(0)
+    }
+    return outputAmount
   }
 
   public snapshot(): PairMentoV2Snapshot {
@@ -124,16 +167,18 @@ export class PairMentoV2 extends Pair {
       bucket0: this.bucket0.toFixed(),
       bucket1: this.bucket1.toFixed(),
       tokenPrecisionMultipliers: this.tokenPrecisionMultipliers.map((v) => v.toFixed()),
+      decimals: this.decimals,
     };
   }
 
   public async restore(snapshot: PairMentoV2Snapshot): Promise<void> {
-    this.spread = new BigNumber(snapshot.spread);
-    this.updateFrequency = new BigNumber(snapshot.updateFrequency);
-    this.pricingModule = snapshot.pricingModule;
-    this.bucket0 = new BigNumber(snapshot.bucket0);
-    this.bucket1 = new BigNumber(snapshot.bucket1);
+    this.spread = new BigNumber(snapshot.spread)
+    this.updateFrequency = new BigNumber(snapshot.updateFrequency)
+    this.pricingModule = snapshot.pricingModule
+    this.bucket0 = new BigNumber(snapshot.bucket0)
+    this.bucket1 = new BigNumber(snapshot.bucket1)
     this.tokenPrecisionMultipliers = snapshot.tokenPrecisionMultipliers.map((v) => new BigNumber(v))
+    this.decimals = snapshot.decimals
   }
 
   private mentoBucketsAfterUpdate = async () => {
@@ -156,9 +201,9 @@ export class PairMentoV2 extends Pair {
         this.poolExchange.config.referenceRateFeedID
       );
     let bucket1 = exchangeRateDenominator
-      .multipliedBy(new BigNumber(bucket0))
-      .idiv(exchangeRateNumerator);
-    return { bucket0, bucket1 };
+      .multipliedBy(bucket0)
+      .idiv(exchangeRateNumerator)
+    return { bucket0, bucket1 }
   };
 
   protected getOracleExchangeRate = async (
@@ -168,11 +213,9 @@ export class PairMentoV2 extends Pair {
       this.sortedOraclesAddress,
       this.provider
     );
-    const [rateNumerator, rateDenominator] = await sortedOracles.medianRate(
-      rateFeedID
-    );
+    const [rateNumerator, rateDenominator] = await sortedOracles.medianRate(rateFeedID)
     if (rateDenominator.lte(0)){
-      throw new Error("exchange rate denominator must be greater than 0");
+      throw new Error("exchange rate denominator must be greater than 0")
     }
     return [
       new BigNumber(rateNumerator._hex),
