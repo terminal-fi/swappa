33d370ee606c1bb745b254433b56d38789d8d574
Zviad Metreveli <thezviad@gmail.com>
2023-09-14 22:32:01 +0000

    mento-v2: filter pairs before initialization

--- src/registries/mento-v2.ts
+++ src/registries/mento-v2.ts
@@ -4,6 +4,7 @@ import { Mento } from "@mento-protocol/mento-sdk";
 import { ethers, providers } from "ethers";
 import { Registry } from "../registry";
 import { PairMentoV2 } from "../pairs/mento-v2";
+import { Address } from "../pair";
 
 export class RegistryMentoV2 extends Registry {
   private provider: ethers.providers.Provider;
@@ -13,14 +14,15 @@ export class RegistryMentoV2 extends Registry {
     this.provider = new providers.Web3Provider(kit.web3.currentProvider as any);
   }
 
-  findPairsWithoutInitialzing = async () => {
+  findPairsWithoutInitialzing = async (tokenWhitelist: Address[]) => {
     const chainId = await this.kit.web3.eth.getChainId();
     const sortedOracelsAddress = await this.kit.registry.addressFor(CeloContract.SortedOracles)
     const mento = await Mento.create(this.provider);
-    const exchanges = await mento.getExchanges();
+    const exchanges = (await mento.getExchanges()).filter(
+      (e) => e.assets.every((asset) => tokenWhitelist.indexOf(asset) >= 0))
     const pairs: PairMentoV2[] = exchanges.map(
       (exchange) => new PairMentoV2(chainId, this.kit.web3 as unknown as Web3, mento, exchange, sortedOracelsAddress)
-    );
+    )
     return pairs
   }
 }
