ef152697fb514956cffa3dc9a9ef2cf976174b40
Zviad Metreveli <thezviad@gmail.com>
2023-12-02 02:21:07 +0000

    create new provider for each mento v2

--- src/registries/mento-v2.ts
+++ src/registries/mento-v2.ts
@@ -7,17 +7,15 @@ import { PairMentoV2 } from "../pairs/mento-v2";
 import { Address } from "../pair";
 
 export class RegistryMentoV2 extends Registry {
-  private provider: ethers.providers.Provider;
 
   constructor(private kit: ContractKit) {
     super("mento-v2");
-    this.provider = new providers.Web3Provider(kit.web3.currentProvider as any);
   }
 
   findPairsWithoutInitialzing = async (tokenWhitelist: Address[]) => {
     const chainId = await this.kit.web3.eth.getChainId();
     const sortedOracelsAddress = await this.kit.registry.addressFor(CeloContract.SortedOracles)
-    const mento = await Mento.create(this.provider);
+    const mento = await Mento.create(new providers.Web3Provider(this.kit.web3.currentProvider as any));
     const exchanges = (await mento.getExchanges()).filter(
       (e) => e.assets.every((asset) => tokenWhitelist.indexOf(asset) >= 0))
     const pairs: PairMentoV2[] = exchanges.map(
