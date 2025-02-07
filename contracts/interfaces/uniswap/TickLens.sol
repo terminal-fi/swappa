90e67196c7ac566ff6609e7f3b2d30cfa2cdb97d
Zviad Metreveli <thezviad@gmail.com>
2025-02-07 17:50:15 +0000

    faster refreshing for univ3 pairs

--- contracts/interfaces/uniswap/TickLens.sol
+++ contracts/interfaces/uniswap/TickLens.sol
@@ -15,7 +15,10 @@ library TickLens {
     // getPoolTicks returns populated ticks from the Uniswapv3 pool.
     // To keep things reasonably performant it only tries to fetch 5 populated tick words.
     // populatedTicks are not sorted, thus it is up to the user to sort them appropriately.
-    function getPoolTicks(IUniswapV3Pool pool, int16 maxLoopN)
+    function getPoolTicks(
+        IUniswapV3Pool pool,
+        int16 maxLoopN
+    )
         internal
         view
         returns (
@@ -31,12 +34,16 @@ library TickLens {
     {
         (
             sqrtPriceX96,
-            tick,
-            , // uint16 observationIndex
-            , // uint16 observationCardinality
-            , // uint16 observationCardinalityNext
-            , // uint8 feeProtocol
+            tick, // uint16 observationIndex
+            // uint16 observationCardinality
+            // uint16 observationCardinalityNext
+            // uint8 feeProtocol
             // bool unlocked
+            ,
+            ,
+            ,
+            ,
+
         ) = pool.slot0();
         liquidity = pool.liquidity();
 
@@ -50,20 +57,41 @@ library TickLens {
         // get the populated ticks near current tick.
         int16 boundTickBitmapIndex = tickBitmapIndex + maxLoopN / 2;
         int16 nextBitmapIndex = tickBitmapIndex;
-        (populatedTicks0, nextBitmapIndex) = nextPopulatedTick(pool, nextBitmapIndex, boundTickBitmapIndex);
-        (populatedTicks1, nextBitmapIndex) = nextPopulatedTick(pool, nextBitmapIndex, boundTickBitmapIndex);
-        (populatedTicks2, nextBitmapIndex) = nextPopulatedTick(pool, nextBitmapIndex, boundTickBitmapIndex);
+        (populatedTicks0, nextBitmapIndex) = nextPopulatedTick(
+            pool,
+            nextBitmapIndex,
+            boundTickBitmapIndex
+        );
+        (populatedTicks1, nextBitmapIndex) = nextPopulatedTick(
+            pool,
+            nextBitmapIndex,
+            boundTickBitmapIndex
+        );
+        (populatedTicks2, nextBitmapIndex) = nextPopulatedTick(
+            pool,
+            nextBitmapIndex,
+            boundTickBitmapIndex
+        );
 
         boundTickBitmapIndex = tickBitmapIndex - maxLoopN / 2;
         nextBitmapIndex = tickBitmapIndex - 1;
-        (populatedTicks3, nextBitmapIndex) = prevPopulatedTick(pool, nextBitmapIndex, boundTickBitmapIndex);
-        (populatedTicks4, nextBitmapIndex) = prevPopulatedTick(pool, nextBitmapIndex, boundTickBitmapIndex);
+        (populatedTicks3, nextBitmapIndex) = prevPopulatedTick(
+            pool,
+            nextBitmapIndex,
+            boundTickBitmapIndex
+        );
+        (populatedTicks4, nextBitmapIndex) = prevPopulatedTick(
+            pool,
+            nextBitmapIndex,
+            boundTickBitmapIndex
+        );
     }
 
     function nextPopulatedTick(
         IUniswapV3Pool pool,
         int16 tickBitmapIndex,
-        int16 maxTickBitmapIndex)
+        int16 maxTickBitmapIndex
+    )
         internal
         view
         returns (PopulatedTick[] memory populatedTicks, int16 nextBitmapIndex)
@@ -71,15 +99,24 @@ library TickLens {
         for (
             nextBitmapIndex = tickBitmapIndex;
             nextBitmapIndex <= maxTickBitmapIndex && populatedTicks.length == 0;
-            nextBitmapIndex += 1) {
-            populatedTicks = getPopulatedTicksInWord(pool, nextBitmapIndex);
+            nextBitmapIndex += 1
+        ) {
+            uint256 bitmap = pool.tickBitmap(nextBitmapIndex);
+            if (bitmap > 0) {
+                populatedTicks = getPopulatedTicksInWord(
+                    pool,
+                    nextBitmapIndex,
+                    bitmap
+                );
+            }
         }
     }
 
     function prevPopulatedTick(
         IUniswapV3Pool pool,
         int16 tickBitmapIndex,
-        int16 minTickBitmapIndex)
+        int16 minTickBitmapIndex
+    )
         internal
         view
         returns (PopulatedTick[] memory populatedTicks, int16 nextBitmapIndex)
@@ -87,19 +124,24 @@ library TickLens {
         for (
             nextBitmapIndex = tickBitmapIndex;
             nextBitmapIndex >= minTickBitmapIndex && populatedTicks.length == 0;
-            nextBitmapIndex -= 1) {
-            populatedTicks = getPopulatedTicksInWord(pool, nextBitmapIndex);
+            nextBitmapIndex -= 1
+        ) {
+            uint256 bitmap = pool.tickBitmap(nextBitmapIndex);
+            if (bitmap > 0) {
+                populatedTicks = getPopulatedTicksInWord(
+                    pool,
+                    nextBitmapIndex,
+                    bitmap
+                );
+            }
         }
     }
 
-    function getPopulatedTicksInWord(IUniswapV3Pool pool, int16 tickBitmapIndex)
-        internal
-        view
-        returns (PopulatedTick[] memory populatedTicks)
-    {
-        // fetch bitmap
-        uint256 bitmap = pool.tickBitmap(tickBitmapIndex);
-
+    function getPopulatedTicksInWord(
+        IUniswapV3Pool pool,
+        int16 tickBitmapIndex,
+        uint256 bitmap
+    ) internal view returns (PopulatedTick[] memory populatedTicks) {
         // calculate the number of populated ticks
         uint256 numberOfPopulatedTicks;
         for (uint256 i = 0; i < 256; i++) {
@@ -111,8 +153,10 @@ library TickLens {
         populatedTicks = new PopulatedTick[](numberOfPopulatedTicks);
         for (uint256 i = 0; i < 256; i++) {
             if (bitmap & (1 << i) > 0) {
-                int24 populatedTick = ((int24(tickBitmapIndex) << 8) + int24(i)) * tickSpacing;
-                (uint128 liquidityGross, int128 liquidityNet, , , , , , ) = pool.ticks(populatedTick);
+                int24 populatedTick = ((int24(tickBitmapIndex) << 8) +
+                    int24(i)) * tickSpacing;
+                (uint128 liquidityGross, int128 liquidityNet, , , , , , ) = pool
+                    .ticks(populatedTick);
                 populatedTicks[--numberOfPopulatedTicks] = PopulatedTick({
                     tick: populatedTick,
                     liquidityNet: liquidityNet,
--- contracts/swappa/SwappaRouterV1.sol
+++ contracts/swappa/SwappaRouterV1.sol
@@ -7,117 +7,146 @@ import "./ISwappaPairV1.sol";
 import "./ISwappaRouterV1.sol";
 
 contract SwappaRouterV1 is ISwappaRouterV1 {
-	event Swap(
-		address indexed sender,
-		address to,
-		address indexed input,
-		address indexed output,
-		uint256 inputAmount,
-		uint256 outputAmount
-	);
+    event Swap(
+        address indexed sender,
+        address to,
+        address indexed input,
+        address indexed output,
+        uint256 inputAmount,
+        uint256 outputAmount
+    );
 
-	modifier ensure(uint deadline) {
-		require(deadline >= block.timestamp, 'SwappaRouter: Expired!');
-		_;
-	}
+    modifier ensure(uint deadline) {
+        require(deadline >= block.timestamp, "SwappaRouter: Expired!");
+        _;
+    }
 
-	function getOutputAmount(
-		address[] calldata path,
-		address[] calldata pairs,
-		bytes[] calldata extras,
-		uint256 inputAmount
-	) external override returns (uint256 outputAmount) {
-		outputAmount = inputAmount;
-		for (uint i; i < pairs.length; i++) {
-			outputAmount =
-				ISwappaPairV1(pairs[i]).getOutputAmount(path[i], path[i+1], outputAmount, extras[i]);
-		}
-	}
+    function getOutputAmount(
+        address[] calldata path,
+        address[] calldata pairs,
+        bytes[] calldata extras,
+        uint256 inputAmount
+    ) external override returns (uint256 outputAmount) {
+        outputAmount = inputAmount;
+        for (uint i; i < pairs.length; i++) {
+            outputAmount = ISwappaPairV1(pairs[i]).getOutputAmount(
+                path[i],
+                path[i + 1],
+                outputAmount,
+                extras[i]
+            );
+        }
+    }
 
-	function swapExactInputForOutput(
-		address[] calldata path,
-		address[] calldata pairs,
-		bytes[] calldata extras,
-		uint256 inputAmount,
-		uint256 minOutputAmount,
-		address to,
-		uint deadline
-	) external override ensure(deadline) returns (uint256 outputAmount) {
-		require(path.length == pairs.length + 1 , "SwappaRouter: Path and Pairs mismatch!");
-		require(pairs.length == extras.length, "SwappaRouter: Pairs and Extras mismatch!");
-		require(pairs.length > 0, "SwappaRouter: Must have at least one pair!");
+    function swapExactInputForOutput(
+        address[] calldata path,
+        address[] calldata pairs,
+        bytes[] calldata extras,
+        uint256 inputAmount,
+        uint256 minOutputAmount,
+        address to,
+        uint deadline
+    ) external override ensure(deadline) returns (uint256 outputAmount) {
+        require(
+            path.length == pairs.length + 1,
+            "SwappaRouter: Path and Pairs mismatch!"
+        );
+        require(
+            pairs.length == extras.length,
+            "SwappaRouter: Pairs and Extras mismatch!"
+        );
+        require(pairs.length > 0, "SwappaRouter: Must have at least one pair!");
 
-		require(
-			ERC20(path[0]).transferFrom(msg.sender, pairs[0], inputAmount),
-			"SwappaRouter: Initial transferFrom failed!");
-		for (uint i; i < pairs.length; i++) {
-			(address pairInput, address pairOutput) = (path[i], path[i + 1]);
-			address next = i < pairs.length - 1 ? pairs[i+1] : address(this);
-			bytes memory data = extras[i];
-			ISwappaPairV1(pairs[i]).swap(pairInput, pairOutput, next, data);
-		}
-		// Perform final output check in the router as a final safeguard to make sure
-		// minOutputAmount restriction is honored no matter what.
-		address output = path[path.length - 1];
-		outputAmount = ERC20(output).balanceOf(address(this));
-		require(
-			outputAmount >= minOutputAmount, "SwappaRouter: Insufficient output amount!");
-		require(
-			ERC20(output).transfer(to, outputAmount),
-			"SwappaRouter: Final transfer failed!");
-		emit Swap(msg.sender, to, path[0], output, inputAmount, outputAmount);
-	}
+        require(
+            ERC20(path[0]).transferFrom(msg.sender, pairs[0], inputAmount),
+            "SwappaRouter: Initial transferFrom failed!"
+        );
+        for (uint i; i < pairs.length; i++) {
+            (address pairInput, address pairOutput) = (path[i], path[i + 1]);
+            address next = i < pairs.length - 1 ? pairs[i + 1] : address(this);
+            bytes memory data = extras[i];
+            ISwappaPairV1(pairs[i]).swap(pairInput, pairOutput, next, data);
+        }
+        // Perform final output check in the router as a final safeguard to make sure
+        // minOutputAmount restriction is honored no matter what.
+        address output = path[path.length - 1];
+        outputAmount = ERC20(output).balanceOf(address(this));
+        require(
+            outputAmount >= minOutputAmount,
+            "SwappaRouter: Insufficient output amount!"
+        );
+        require(
+            ERC20(output).transfer(to, outputAmount),
+            "SwappaRouter: Final transfer failed!"
+        );
+        emit Swap(msg.sender, to, path[0], output, inputAmount, outputAmount);
+    }
 
-	function swapExactInputForOutputWithPrecheck(
-		address[] calldata path,
-		address[] calldata pairs,
-		bytes[] calldata extras,
-		uint256 inputAmount,
-		uint256 minOutputAmount,
-		address to,
-		uint deadline
-	) external override ensure(deadline) returns (uint256 outputAmount) {
-		require(path.length == pairs.length + 1 , "SwappaRouter: Path and Pairs mismatch!");
-		require(pairs.length == extras.length, "SwappaRouter: Pairs and Extras mismatch!");
-		require(pairs.length > 0, "SwappaRouter: Must have at least one pair!");
+    function swapExactInputForOutputWithPrecheck(
+        address[] calldata path,
+        address[] calldata pairs,
+        bytes[] calldata extras,
+        uint256 inputAmount,
+        uint256 minOutputAmount,
+        address to,
+        uint deadline
+    ) external override ensure(deadline) returns (uint256 outputAmount) {
+        require(
+            path.length == pairs.length + 1,
+            "SwappaRouter: Path and Pairs mismatch!"
+        );
+        require(
+            pairs.length == extras.length,
+            "SwappaRouter: Pairs and Extras mismatch!"
+        );
+        require(pairs.length > 0, "SwappaRouter: Must have at least one pair!");
 
-		// Note(zviadm): Full code copying is necessary because `bytes[]` arrays can not easily be
-		// passed around between `calldata` and `memory` locations. Manual copying would be necessary
-		// with quite a bit of annoying code to work around stack issues.
+        // Note(zviadm): Full code copying is necessary because `bytes[]` arrays can not easily be
+        // passed around between `calldata` and `memory` locations. Manual copying would be necessary
+        // with quite a bit of annoying code to work around stack issues.
 
-		{
-			outputAmount = inputAmount; // reuse outputAmount variable to avoid "stack too deep" errors.
-			for (uint i; i < pairs.length; i++) {
-				(address input, address output) = (path[i], path[i+1]);
-				bytes memory data = extras[i];
-				outputAmount =
-					ISwappaPairV1(pairs[i]).getOutputAmount(input, output, outputAmount, data);
-			}
-			require(
-				outputAmount >= minOutputAmount,
-				"SwappaRouter: Insufficient expected output amount!");
-		}
+        {
+            outputAmount = inputAmount; // reuse outputAmount variable to avoid "stack too deep" errors.
+            for (uint i; i < pairs.length; i++) {
+                (address input, address output) = (path[i], path[i + 1]);
+                bytes memory data = extras[i];
+                outputAmount = ISwappaPairV1(pairs[i]).getOutputAmount(
+                    input,
+                    output,
+                    outputAmount,
+                    data
+                );
+            }
+            require(
+                outputAmount >= minOutputAmount,
+                "SwappaRouter: Insufficient expected output amount!"
+            );
+        }
 
-		require(
-			ERC20(path[0]).transferFrom(msg.sender, pairs[0], inputAmount),
-			"SwappaRouter: Initial transferFrom failed!");
-		for (uint i; i < pairs.length; i++) {
-			(address pairInput, address pairOutput) = (path[i], path[i + 1]);
-			address next = i < pairs.length - 1 ? pairs[i+1] : address(this);
-			bytes memory data = extras[i];
-			ISwappaPairV1(pairs[i]).swap(pairInput, pairOutput, next, data);
-		}
-		// Perform final output check in the router as a final safeguard to make sure
-		// minOutputAmount restriction is honored no matter what.
-		address output = path[path.length - 1];
-		outputAmount = ERC20(output).balanceOf(address(this));
-		require(
-			outputAmount >= minOutputAmount, "SwappaRouter: Insufficient output amount!");
-		require(
-			ERC20(output).transfer(to, outputAmount),
-			"SwappaRouter: Final transfer failed!");
-		emit Swap(msg.sender, to, path[0], output, inputAmount, outputAmount);
-	}
+        require(
+            ERC20(path[0]).transferFrom(msg.sender, pairs[0], inputAmount),
+            "SwappaRouter: Initial transferFrom failed!"
+        );
+        for (uint i; i < pairs.length; i++) {
+            (address pairInput, address pairOutput) = (path[i], path[i + 1]);
+            address next = i < pairs.length - 1 ? pairs[i + 1] : address(this);
+            bytes memory data = extras[i];
+            ISwappaPairV1(pairs[i]).swap(pairInput, pairOutput, next, data);
+        }
+        // Perform final output check in the router as a final safeguard to make sure
+        // minOutputAmount restriction is honored no matter what.
+        address output = path[path.length - 1];
+        outputAmount = ERC20(output).balanceOf(address(this));
+        require(
+            outputAmount >= minOutputAmount,
+            "SwappaRouter: Insufficient output amount!"
+        );
+        require(
+            ERC20(output).transfer(to, outputAmount),
+            "SwappaRouter: Final transfer failed!"
+        );
+        emit Swap(msg.sender, to, path[0], output, inputAmount, outputAmount);
+    }
 
-	receive() external payable {}
+    receive() external payable {}
 }
